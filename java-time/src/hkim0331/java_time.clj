a84bcdd318f771d542ad24bf19172ee588c8a055
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-07 10:08:32 +0900

    updated javatime

--- java-time/src/hkim0331/java_time.clj
+++ java-time/src/hkim0331/java_time.clj
@@ -3,7 +3,21 @@
    [clojure.string :as str]
    [java-time :as jt]))
 
-;; epoch = unix-time
+(defn now-in-milli
+  "returns milli from epoch"
+  []
+  (-> (jt/instant)
+      jt/to-millis-from-epoch))
+
+(defn now-in-epoch
+  "returns seconds from epoch"
+  []
+  (-> (now-in-milli)
+      (quot 1000)))
+
+;; % date -d '@1662467696'
+;; % date -u -d '@1662467696'
+
 ;; str->milli is a better name for this function?
 ;; since the argument is not a datetime object.
 (defn datetime->milli
@@ -17,8 +31,6 @@
 
 ;; (java.time.Instant/ofEpochMilli 1661330819000))
 ;; #object[java.time.Instant 0x6f6a7463 "2022-08-24T08:46:59Z"]
-;; (str *1)
-;; "2022-08-24T08:46:59Z"
 
 (defn datetime->epoch
   [s]
@@ -34,24 +46,12 @@
                     jt/local-date-time
                     (jt/format fmt))))
 
+
 (comment
   (datetime->epoch "2022-08-25 12:34:56")
   (let [ep (datetime->epoch "2022-08-25 12:34:56")]
-    (epoch->datetime ep)))
-
-(defn now-in-milli
-  "returns milli from epoch"
-  []
-  (-> (jt/instant)
-      jt/to-mills-from-epoch))
-
-(defn now-in-epoch
-  "returns seconds from epoch"
-  []
-  (-> (now-in-milli)
-      (quot 1000)))
-
-(comment
+    (epoch->datetime ep))
   (dotimes [_ 5]
     (println (epoch->datetime (now-in-epoch)))
     (Thread/sleep 1000)))
+
