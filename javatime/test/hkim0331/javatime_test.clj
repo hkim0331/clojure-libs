d42c8cf70f2f5101e0f8595b878fcfa124d6c132
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-10 10:44:36 +0900

    why (javatime/milli->str 0) returns "1970-01-01 09:00:01"?

--- javatime/test/hkim0331/javatime_test.clj
+++ javatime/test/hkim0331/javatime_test.clj
@@ -3,9 +3,8 @@
    [clojure.test :refer [deftest testing is]]
    [hkim0331.javatime :refer :all]))
 
-(deftest mill->str-test
-  (testing "mill->str"
+;; WHY?
+(deftest milli->str-test
+  (testing "milli->str"
     (is (= (str (milli->str 0))
-           "1970-01-01T09:00"))))
-
-;; (epoch->str "yyyy/MM/dd hh:mm" 1661330819)
+           "1970-01-01 09:00:01"))))
